9b93d88a7aa9f5f727c8a5610d33122de75843a4
jacek_skorzynski <jacek@redhat.com>
2023-05-09 09:34:41 +0200

    add public ip option

--- main.tf
+++ main.tf
@@ -6,13 +6,9 @@ terraform {
     }
   }
 }
+
 provider "azurerm" {
   features {}
-
-#   client_id       = "${var.azure_client_id}" 
-#   client_secret   = "${var.azure_client_secret}" 
-#   tenant_id       = "${var.azure_tenant_id}" 
-#   subscription_id = "${var.azure_subscription_id}" 
 }
 
 resource "azurerm_resource_group" "main" {
@@ -34,10 +30,30 @@ resource "azurerm_subnet" "internal" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
+resource "azurerm_public_ip" "pip" {
+  name                = "${var.prefix}-pip"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic1"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  ip_configuration {
+    name                          = "primary"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_network_interface" "internal" {
+  name                      = "${var.prefix}-nic2"
+  resource_group_name       = azurerm_resource_group.main.name
+  location                  = azurerm_resource_group.main.location
 
   ip_configuration {
     name                          = "internal"
@@ -46,22 +62,40 @@ resource "azurerm_network_interface" "main" {
   }
 }
 
+resource "azurerm_network_security_group" "webserver" {
+  name                = "ssh_port"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  security_rule {
+    access                     = "Allow"
+    direction                  = "Inbound"
+    name                       = "ssh"
+    priority                   = 100
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    source_address_prefix      = "*"
+    destination_port_range     = "22"
+    destination_address_prefix = azurerm_network_interface.main.private_ip_address
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "main" {
+  network_interface_id      = azurerm_network_interface.internal.id
+  network_security_group_id = azurerm_network_security_group.webserver.id
+}
+
 resource "azurerm_linux_virtual_machine" "main" {
   name                            = "${var.prefix}-vm"
   resource_group_name             = azurerm_resource_group.main.name
   location                        = azurerm_resource_group.main.location
   size                            = "Standard_A2_v2"
   admin_username                  = "ansible"
-  admin_password                  = "Ansible123"
+  admin_password                  = "Ansible1234!"
   disable_password_authentication = false
   network_interface_ids = [
     azurerm_network_interface.main.id,
+    azurerm_network_interface.internal.id,
   ]
-
-#   admin_ssh_key {
-#     username = "ansible"
-#     public_key = file("~/.ssh/id_rsa.pub")
-#   }
 
   source_image_reference {
     publisher = "RedHat"
